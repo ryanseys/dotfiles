f04a4edf23f158708aafd2536760b68f4805a816
Ryan Seys <ryan.seys@shopify.com>
2021-04-14 09:55:46 -0700

    Update dotfiles to use oh my zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh-history-substring-search
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy dotfiles into their places
 cp aliases ~/.aliases
 cp hushlogin ~/.hushlogin
 cp gitignore_global ~/.gitignore_global
--- zshrc
+++ zshrc
@@ -1,5 +1,15 @@
 #!/bin/zsh
 
-source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/.zsh-history-substring-search/zsh-history-substring-search.zsh
+# Path to your oh-my-zsh installation.
+export ZSH="~/.oh-my-zsh"
+
 source ~/.aliases
+
+ZSH_THEME="robbyrussell"
+DISABLE_UPDATE_PROMPT="true"
+
+plugins=(git history-substring-search zsh-autosuggestions zsh-completions ruby zsh-syntax-highlighting)
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=7'
+
+source $ZSH/oh-my-zsh.sh
