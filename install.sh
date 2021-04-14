16f5c044b2420f62fb8ac62c890b208167b8a370
Ryan Seys <ryan.seys@shopify.com>
2021-04-14 09:25:38 -0700

    Update dotfiles with zsh plugins

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh-history-substring-search
+
 cp aliases ~/.aliases
 cp hushlogin ~/.hushlogin
 cp gitignore_global ~/.gitignore_global
 cp vimrc ~/.vimrc
 cp gitconfig ~/.gitconfig
+cp zshrc ~/.zshrc
---
+++ zshrc
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh-history-substring-search/zsh-history-substring-search.zsh
+source ~/.aliases
