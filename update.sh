b578cf030b833bdd5f0fe27682875cb2b5d6d901
Ryan Seys <ryan.seys@shopify.com>
2022-02-04 16:30:00 -0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Copy dotfiles into their places
+cp ~/.aliases aliases
+cp ~/.hushlogin hushlogin
+cp ~/.gitignore_global gitignore_global
+cp ~/.vimrc vimrc
+cp ~/.gitconfig gitconfig
+cp ~/.zshrc zshrc
+
+# Copy zsh theme into right place.
+cp ~/.oh-my-zsh/themes/ryanseys.zsh-theme ryanseys.zsh-theme
